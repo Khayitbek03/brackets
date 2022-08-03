d448b186d386b403141e341e1ea75822385f945c
Khayitbek03 <xayitbekmirsoatov@gmail.com>
2022-08-03 21:51:56 +0500

    init:add js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+const open_bracket=[];
+const pair_bracket={};
+bracketsConfig.forEach(elem=>{
+  open_bracket.push(elem[0]);
+  pair_bracket[elem[1]]=elem[0];
+})
+let stack=[];
+for(let i=0;i<str.length;i++){
+  let currentSymbol=str[i]//first symbol in the string
+  let topSymbol=stack[stack.length-1];//last symbol in the stack;
+  if(open_bracket.includes(currentSymbol)){
+    if(topSymbol===currentSymbol && pair_bracket[currentSymbol]===currentSymbol){
+      stack.pop();
+    }
+    else{
+      stack.push(currentSymbol);
+    }
+  }
+  else{
+    if(stack.length===0){
+      return false;
+    }
+ 
+  if(pair_bracket[currentSymbol]===topSymbol){
+    stack.pop();
+  }
+  else{
+    return false;
+  }
+}
+}
+  return stack.length===0;
 }
